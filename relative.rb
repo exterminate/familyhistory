2cff1df71d80cdbc9c7fb0141f2d5d6c0566556e
Ian <ian.coates@gmail.com>
2015-05-19 13:30:48 +0100

    update

--- relative.rb
+++ relative.rb
@@ -1,5 +1,16 @@
 class Relative
-
-
-
+	
+	def initialize(firstname, lastname, dateofbirth)
+		@firstname   = firstname
+		@lastname    = lasname
+		@dateofbirth = dateofbirth
+	end
+	
+	def add_dateofdeath(dateofdeath)
+		
+	end
+	
+	def occupation(occupation)
+		@occupation = occupation
+	end
 end
